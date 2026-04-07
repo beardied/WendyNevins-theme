c170b8c4789569dcfc6b9f23396e42790e36bae3
root <root@host.orangewidow.com>
2026-04-07 06:55:57 +0100

    Bump version to 1.4.1 for cache busting

--- functions.php
+++ functions.php
@@ -12,7 +12,7 @@ if (!defined('ABSPATH')) {
 
 // Theme version for cache busting
 if (!defined('WENDYNEVINS_VERSION')) {
-    define('WENDYNEVINS_VERSION', '1.0.0');
+    define('WENDYNEVINS_VERSION', '1.4.1');
 }
 
 /**
