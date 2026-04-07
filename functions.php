13a0ca0ec021f7f1f94b8d8c978fc53fc998d5ed
root <root@host.orangewidow.com>
2026-04-07 10:17:53 +0100

    Fix CSS version constant for cache busting 1.5.1

--- functions.php
+++ functions.php
@@ -12,7 +12,7 @@ if (!defined('ABSPATH')) {
 
 // Theme version for cache busting
 if (!defined('WENDYNEVINS_VERSION')) {
-    define('WENDYNEVINS_VERSION', '1.4.7');
+    define('WENDYNEVINS_VERSION', '1.5.1');
 }
 
 /**
