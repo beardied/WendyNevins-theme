9acb5629d874b151ae7f3eab42175feb96c8483d
root <root@host.orangewidow.com>
2026-04-07 07:40:48 +0100

    v1.4.4: Ensure SVG icon is visible with white stroke

--- functions.php
+++ functions.php
@@ -12,7 +12,7 @@ if (!defined('ABSPATH')) {
 
 // Theme version for cache busting
 if (!defined('WENDYNEVINS_VERSION')) {
-    define('WENDYNEVINS_VERSION', '1.4.3');
+    define('WENDYNEVINS_VERSION', '1.4.4');
 }
 
 /**
