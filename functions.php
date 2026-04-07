94899ea160f2f92a9b8ae1bdea910c0b2c32b998
root <root@host.orangewidow.com>
2026-04-07 07:20:48 +0100

    v1.4.3: Force search button icon with CSS - hide text, show magnifying glass

--- functions.php
+++ functions.php
@@ -12,7 +12,7 @@ if (!defined('ABSPATH')) {
 
 // Theme version for cache busting
 if (!defined('WENDYNEVINS_VERSION')) {
-    define('WENDYNEVINS_VERSION', '1.4.2');
+    define('WENDYNEVINS_VERSION', '1.4.3');
 }
 
 /**
